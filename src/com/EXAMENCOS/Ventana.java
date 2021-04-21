a7fd2cd3d47ac4f6c6abad2b774e65b674d4fd01
plagocastro <plagocastro@danielcastelao.org>
2021-04-21 19:59:49 +0200

    añadimos la clase Ventana

--- src/com/EXAMENCOS/Ventana.java
+++ src/com/EXAMENCOS/Ventana.java
@@ -1,4 +1,18 @@
 package com.EXAMENCOS;
 
-public class Ventana {
+import javax.swing.JOptionPane;
+
+
+public class Ventana implements InterfaceMetodosComunes {
+    private String msg;
+
+
+    public Ventana(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void visualiza(){
+        JOptionPane.showMessageDialog(null,"Sale por un ventana " + msg);
+    }
 }
