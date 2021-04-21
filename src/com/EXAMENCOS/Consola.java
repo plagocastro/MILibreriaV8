8bdebaf9351fd87c177223db9fd2b4dc3ac64682
plagocastro <plagocastro@danielcastelao.org>
2021-04-21 20:00:07 +0200

    añadimos la clase Consola

--- src/com/EXAMENCOS/Consola.java
+++ src/com/EXAMENCOS/Consola.java
@@ -1,4 +1,18 @@
 package com.EXAMENCOS;
 
 public class Consola {
+
+    public class Consola implements InterfaceMetodosComunes {
+        private String msg;
+
+
+        public Consola(String msg){
+            this.msg=msg;
+        }
+
+        @Override
+        public void visualiza(){
+            System.out.println("Sale por la consola " + msg);
+        }
+    }
 }
